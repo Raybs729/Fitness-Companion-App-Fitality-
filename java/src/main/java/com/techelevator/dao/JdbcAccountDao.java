87d7f98e8a87ac59712858918cbaf12d72688098
Hewad Noori <hewadnoori16@gmail.com>
2023-04-16 15:15:04 -0400

    changed account sql

--- java/src/main/java/com/techelevator/dao/JdbcAccountDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcAccountDao.java
@@ -62,10 +62,10 @@ public class JdbcAccountDao implements AccountDao {
     @Override   /***NEW***/
     public boolean updateUserInfo(Account account) {
         String sql = "UPDATE public.account" +
-        "\t SET user_id=?, first_name=?, last_name=?, email=?, phone=?, \n" +
+        "\t SET first_name=?, last_name=?, email=?, phone=?, \n" +
         "\t age=?, height=?, weight= ?, goals= ?, photo= ? \n" +
         "\t WHERE user_id= ?; ";
-        jdbcTemplate.update(sql, account.getFirstName(),account.getLastName(),account.getEmail(),account.getPhone(), account.getAge(), account.getHeight(),account.getGoals(), account.getPhoto());
+        jdbcTemplate.update(sql, account.getFirstName(),account.getLastName(),account.getEmail(),account.getPhone(), account.getAge(), account.getHeight(), account.getWeight(), account.getGoals(), account.getPhoto());
          return true;
     }
 
