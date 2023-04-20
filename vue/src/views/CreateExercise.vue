8e70bb1857618abebca95c9f6630970860c59c71
Hewad Noori <hewadnoori16@gmail.com>
2023-04-19 23:51:58 -0400

    create exercise

--- vue/src/views/CreateExercise.vue
+++ vue/src/views/CreateExercise.vue
@@ -1,41 +1,21 @@
 <template>
-  <div class="nav">
+  <div class="exercise">
     <div class="title">
-      <h1>Fitality</h1>
-      <h2> <span> Exercise</span><span class="bg-text">New</span></h2>
-    </div>
-  
-    <!-- <img id="inverted-logo" src="../img/invertedlogo.png" alt="inverted logo"> -->
-     <!-- <img id="background-img" src="../img/createExerciseVue.jpeg">  -->
+        <h3>CREATE EXERCISE</h3>
+      </div>
     <div class="input">
       <form @submit.prevent="submitForm">
-        <div class="table">
-          <div class="info1">
-                <div class="information1 ">
-                  <div class="box">
-                    <label id="exercise-name">Exercise Name:</label>
-                  </div>
-                  <div>
-                    <input v-model="newExercise.exerciseName" required />
-                  </div>
-                </div>  
-            </div>
-            <div class="info1">
-                <div class="information1 ">
-                  <div class="box">
-                    <label>Equipment Name:</label>
-                  </div>
-                  <div>
-                    <select v-model="newExercise.equipmentName" required>
-                      <option v-for="equipment in equipments" :key="equipment.equipmentId" :value="equipment.equipmentName">{{ equipment.equipmentName }}</option>
-                    </select>
-                  </div>
-                </div>
-          </div>
+        <div class="section">
+          <label id="exercise-name">Exercise Name</label>
+          <input v-model="newExercise.exerciseName" required />
         </div>
-        <div class="button">
+        <div class="section">
+          <label>Equipment Name</label>
+          <select v-model="newExercise.equipmentName" required>
+            <option v-for="equipment in equipments" :key="equipment.equipmentId" :value="equipment.equipmentName">{{ equipment.equipmentName }}</option>
+          </select>
+        </div>  
           <button type="submit">Submit</button>
-        </div> 
       </form>
     </div>
   </div>
@@ -91,169 +71,68 @@ export default {
 
 <style scoped>
 
-/* Inverted Logo  */
-
-#inverted-logo {
-position: absolute;
-left: 10px;
-top: 28px;
-z-index: -2;
-width: 20%;
-height: 10%;
-background: #ffffff;
-
-} 
-
-/* Fitality Header */
-
-.title {
-  text-align: center;
-  font-family: 'Poppins', serif;
-}
-
-h1 {
-  position: absolute;
-  top: -1%;
-  left: 5%;
-  
-  transform: translate(50%, -50%);
-  font-size: 7rem;
-  z-index: 0;
-  color: rgba(255, 255, 255, 0.5); 
-  text-shadow: 0 0 10px rgba(0, 0, 0, 0.5); 
-  animation: rotate 3s linear forwards;
-}
-
-@keyframes rotate {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(320deg);
-  }
-}
-
-/* form {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-} */
-h2 {
-position: relative;
-text-transform: uppercase;
-font-size: 4rem;
-font-weight: 950;
-padding-left: 10px;
-z-index: 1;
-}
-.bg-text{
-  font-family: 'Poppins', sans-serif;
-  position: absolute;
-  top: 10%;
-  left: 15%;
-  color: #76736d;
-  transition: all  .4s ease-in-out;
-       z-index: 0;
-       transform: translate(-50%, -50%);
-       font-weight: 700;
-       font-size: 3rem;
-}
-
-span {
-  color: #d8950e;
-  z-index: 2;
-}
-
-.table {
-  width: 100%;
-  height: 80%;
-  padding-top: 50px;
+.exercise {
+  height: 100vh;
+  background-color: #e7e6e9;
   display: flex;
   flex-direction: column;
   align-items: center;
-  z-index: 2;
-}
-
-.info1 { 
-  height: 100%;
-  width: 90%;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  padding-bottom: 10px;
-  z-index: 2;
-}
-
-/* Exercise Input */
-
-.information1 {
-  display: flex;
-  left: 50%;
-  justify-content: space-between;
-  align-items: center;
-  border: 1px solid black;
-  border-radius: 5px;
-  background-color: rgb(255, 255, 255);
-  box-shadow: 0 5px  25px rgba(0,0,0,.5);
-  transition: all .6s ease-in-out;
-  font-family: Georgia, 'Times New Roman', Times, serif;
-  font-size: 15px;
   width: 100%;
-  padding: 15px 25px 15px 25px;
-  
 }
-.nav {
-  background-color: #f7f3f0;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
+.input{
+  width: 95%;
 }
-
-.button  {
+form{
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 }
-button {
-  width: 200px;
-  height: 30px;
+label {
+  display: inline-block;
+  margin: 0.5em 0;
+}
+option {
+  margin: 0.5em 0;
+}
+.section {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  margin-bottom: 10px;
+  padding: 10px;
+  padding-left: 20px;
   border-radius: 5px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: #47433a;
-  overflow-x: hidden;
-  border: 2px solid #333231;
-  background: #b4afac;
-  font-family: Georgia, 'Times New Roman', Times, serif;
-  font-size: 15px;
-  box-shadow: 0 3px 5px rgba(119, 105, 105, 0.3);
-  transition: all .6s ease-in-out;
-  text-transform: uppercase;
-  
-} 
-.table input , select {
-  padding-left: 5px;
-  margin-left: 50px;
-  height: 30px;
-  width: 180px;
-  border-radius: 10px;
-  overflow-x: hidden;
-  border: 3px solid #a19f99;
-  align-items: center;
-  background: #FFFFFF;
-  box-shadow: 0 3px 15px rgba(0, 0, 0, 0.3);
-  transition: all .6s ease-in-out;
+  width: 95%;
 }
-
-/*** PHONE DISPLAY ***/
-@media (max-width: 480px) {
- 
-
- select {
-  text-transform: uppercase;
- }
+.title{
+  background-color: white;
+  margin-bottom: 10px;
+  text-align: center;
+  width: 100%;
+  font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+  color:rgb(37, 34, 34);
+}
+button{
+  background-color: red;
+  border-radius: 20px;
+  width: 150px;
+  padding: 15px;
+  border:none;
+  color: white;
+  font-size: large ;
+}
+input{
+  width: 60%;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  box-sizing: border-box;
+}
+select{
+  width: 60%;
+  border: none;
+  padding: 10px;
 }
 
 </style>
